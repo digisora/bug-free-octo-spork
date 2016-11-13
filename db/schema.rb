7542eb40f2e223d172878d6058f4d01a2a134e18
digisora <jonkhia@gmail.com>
2016-11-13 20:11:58 +0000

    modified db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830223020) do
+ActiveRecord::Schema.define(version: 20160908212338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160830223020) do
     t.string   "author"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "source"
   end
 
 end
